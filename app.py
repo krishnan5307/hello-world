0a05ac2c6736938d99a7d3cd30548c0d58d6750d
krishnanunni br <krishnanunni5307@gmail.com>
2023-01-16 18:26:09 +0530

    egeg

---
+++ app.py
@@ -0,0 +1,14 @@
+
+from flask import Flask,request,app,jsonify,url_for,render_template
+
+app=Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('template.html')
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
